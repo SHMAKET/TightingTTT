089b44f87ee35ef1e4d8bc024f8b8974005b3aae
SHMAKET <grachovv12345@gmail.com>
2025-08-20 20:15:48 +0300

    delay for duplicate messages

--- src/logger.cpp
+++ src/logger.cpp
@@ -3,10 +3,19 @@
 #include "wifi_manager.h"
 
 void print(String mess) {
-    if (isWiFiConnected() && updateTime()){
-        Serial.println("[" + TimeToString(currentTime) + "] " + mess);
-    }
-    else {
-        Serial.println("[       NoTime      ]  " + mess);
-    }
+    static String lastMessage = "";
+
+    static unsigned long lastTime = 0;
+    unsigned long now = millis();
+    if (mess == lastMessage && (now - lastTime) < 1000)
+        return;
+
+    lastMessage = mess;
+    lastTime = now;
+
+    String time = (isWiFiConnected() && updateTime()) ? 
+        TimeToString(currentTime) : 
+        "       NoTime      ";
+
+    Serial.println("[" + time + "] " + mess);
 }
